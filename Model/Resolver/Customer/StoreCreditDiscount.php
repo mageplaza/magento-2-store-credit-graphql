3e2e260225cc62d74ccf1977c8f580f45dd1a998
ndluong <luongnd@mageplaza.com>
2020-10-15 17:10:09 +0700

    error when get cart price if output result contains mp_store_credit_discount for guest checkout

--- Model/Resolver/Customer/StoreCreditDiscount.php
+++ Model/Resolver/Customer/StoreCreditDiscount.php
@@ -25,21 +25,54 @@ namespace Mageplaza\StoreCreditGraphQl\Model\Resolver\Customer;
 
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Mageplaza\StoreCreditGraphQl\Model\Resolver\AbstractStoreCredit;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Mageplaza\StoreCredit\Helper\Data;
 
 /**
  * Class StoreCreditDiscount
  * @package Mageplaza\StoreCreditGraphQl\Model\Resolver\Customer
  */
-class StoreCreditDiscount extends AbstractStoreCredit
+class StoreCreditDiscount implements ResolverInterface
 {
     /**
-     * @inheritdoc
+     * @var Data
+     */
+    protected $helperData;
+
+    /**
+     * AbstractStoreCredit constructor.
+     *
+     * @param Data $helperData
+     */
+    public function __construct(Data $helperData)
+    {
+        $this->helperData = $helperData;
+    }
+
+    /**
+     * @param Field $field
+     * @param $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     *
+     * @return array[]
+     * @throws GraphQlNoSuchEntityException
+     * @throws LocalizedException
      */
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
     {
-        parent::resolve($field, $context, $info, $value, $args);
+        if (!$this->helperData->isEnabled()) {
+            throw new GraphQlNoSuchEntityException(__('Store Credit is disabled.'));
+        }
+
+        if ($this->helperData->versionCompare('2.3.3')) {
+            if ($context->getExtensionAttributes()->getIsCustomer() === false) {
+                return null;
+            }
+        }
 
         if (!isset($value['model'])) {
             throw new LocalizedException(__('"model" value should be specified'));
